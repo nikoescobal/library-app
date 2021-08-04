52d221385aaf179558aa6957519fdd71bffd9000
Niko Escobal <niko.escobal@gmail.com>
2021-08-05 01:07:45 +0800

    Fix error to display books

--- book.js
+++ book.js
@@ -21,15 +21,7 @@ addBookToLibrary();
 for (var book in myLibrary) {
   console.log(myLibrary[book])
 
-  const bookElement = `<p id="">${book.author + book.title + book.pageCount + book.hasRead}</p>`
+  const bookElement = `<p id="">${myLibrary[book].author + ' ' + myLibrary[book].title + ' ' + myLibrary[book].pageCount + ' ' + myLibrary[book].hasRead}</p>`;
   const bookholder = document.getElementById("bookholder");
-  bookholder.appendChild(bookElement);
-  // document.getElementById("bookholder").innerHTML = bookElement;
-  //document.getElementById("book" + x + "-id").innerText =
-}
-
-for (let index = 0; index < array.length; index++) {
-  const bookElement = `<p id="">${myLibrary[index].author + myLibrary[index].title + myLibrary[index].pageCount + myLibrary[index].hasRead}</p>`
-  const element = array[index];
-
+  bookholder.insertAdjacentHTML('afterbegin', bookElement);
 }
