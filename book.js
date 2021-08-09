df59d265490ea61857e521677fd2f2e956f51e9c
Niko Escobal <niko.escobal@gmail.com>
2021-08-09 19:53:55 +0800

    Add semicolon to fix typo in remove function.

--- book.js
+++ book.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-let myLibrary = [];
+myLibrary = [];
 
 function Book(author, title, pageCount, hasRead) {
   const book = {
@@ -11,15 +10,6 @@ function Book(author, title, pageCount, hasRead) {
   return book;
 }
 
-function clearDisplay() {
-  const bookholder = document.getElementById('bookholder');
-  bookholder.innerHTML = '';
-}
-
-function displayLibrary() {
-  clearDisplay();
-}
-
 function addBookToLibrary() {
   const author = document.getElementById('author').value;
   const title = document.getElementById('title').value;
@@ -28,26 +18,31 @@ function addBookToLibrary() {
   const newBook = Book(author, title, pageCount, hasRead);
   myLibrary.push(newBook);
   displayLibrary();
-
-  const savedLibrary = JSON.stringify(myLibrary);
-  localStorage.setItem('book', savedLibrary);
-  // const book =
-  localStorage.getItem('book');
-  localStorage.clear();
 }
 
 function removeBookFromLibrary(bookTitle) {
-  const newLibrary = myLibrary.filter((book) => book.title !== bookTitle);
+  const newLibrary = myLibrary.filter((book) => {
+    return book.title !== bookTitle;
+  });
   myLibrary = newLibrary;
   displayLibrary();
 }
 
-const bookholder = document.getElementById('bookholder');
+function clearDisplay() {
+  const bookholder = document.getElementById('bookholder');
+  bookholder.innerHTML = '';
+}
 
-myLibrary.forEach((book) => {
-  const bookElement = `<p>${book.author} ${book.title} ${book.pageCount} ${book.hasRead} <button class="remove-book" onClick="removeBookFromLibrary('${book.title}');">x</button></p>`;
-  bookholder.innerHTML += bookElement;
-});
+function displayLibrary() {
+  clearDisplay();
+
+  const bookholder = document.getElementById('bookholder');
+
+  myLibrary.forEach((book) => {
+    const bookElement = `<p>${book.author} ${book.title} ${book.pageCount} ${book.hasRead} <button class="remove-book" onClick="removeBookFromLibrary('${book.title}');">x</button></p>`;
+    bookholder.innerHTML += bookElement;
+  });
+}
 
 document.getElementById('submit').addEventListener('click', () => {
   addBookToLibrary();
